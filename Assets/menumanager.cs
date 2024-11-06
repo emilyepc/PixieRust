42412ac3f22bb2d53663b3ef66a434a826b19f16
CatCultist <infinitesnakes@gmail.com>
2024-11-06 10:31:27 +0000

    grass textures and smoothing+menu

---
+++ Assets/menumanager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menumanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void Playgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void options()
+    {
+
+    }
+   
+}
