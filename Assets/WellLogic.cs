67cc1fefa64f861dec428c7bbc9c372ba4a5b906
GraicDaBoss <grace1gallagher@icloud.com>
2024-11-06 21:13:42 +0000

    yus

---
+++ Assets/WellLogic.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellLogic : MonoBehaviour
+{
+    public GameObject Waterempty, Waterfull, UI, particlesystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial states
+        Waterempty.SetActive(true);
+        Waterfull.SetActive(false);
+        UI.SetActive(false);
+        particlesystem.SetActive(false);
+
+        // Debug messages to confirm initialization
+        Debug.Log("WellLogic initialized. Particle System set to inactive.");
+    }
+
+    // Trigger event when another collider enters the trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player enters the collider and if the particle system is already active
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered trigger.");
+
+            // Checking if particle system is active
+            if (particlesystem.activeSelf)
+            {
+                Debug.Log("Particle system is active. Filling well.");
+
+                // Activate full well, deactivate empty well, and show UI
+                Waterempty.SetActive(false);
+                Waterfull.SetActive(true);
+                UI.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("Particle system is NOT active. Cannot fill well.");
+            }
+        }
+    }
+
+    // Trigger event when the player exits the collider
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the player exits the collider
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited trigger.");
+
+            // Hide UI, if needed
+            UI.SetActive(false);
+        }
+    }
+}
+
