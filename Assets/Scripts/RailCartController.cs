27e22e6c022d379412fe04f66b1207c4599cda78
GraicDaBoss <grace1gallagher@icloud.com>
2025-01-15 17:15:07 +0000

    Revert "uh"
    This reverts commit 83e6e519de9f10b0d96589641f41511750691f33.

--- Assets/Scripts/RailCartController.cs
+++ Assets/Scripts/RailCartController.cs
@@ -9,7 +9,6 @@ public class RailCartController : MonoBehaviour
     public GameObject RailGate;
     public GameObject StaticCart;
     public GameObject MovingCart;
-    public GameObject text;
 
     void Update()
     {
@@ -19,13 +18,11 @@ public class RailCartController : MonoBehaviour
 
             StaticCart.SetActive(true);
             MovingCart.SetActive(false);
-            text.SetActive(false);
         }
         else
         {
             StaticCart.SetActive(false);
             MovingCart.SetActive(true);
-            text.SetActive(true);
         }
     }
 }
